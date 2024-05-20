a893f1fb985a4fb8aa336706f6f64f832a8d6bed
menezesledilson <ledilsonmenezes@gmail.com>
2024-05-20 14:33:38 -0300

    layout

---
+++ layout_adm/js/custom_adm.js
@@ -0,0 +1,24 @@
+// Dropdown Navbar
+let notification = document.querySelector(".notification");
+let avatar = document.querySelector(".avatar");
+
+dropMenu(avatar);
+dropMenu(notification);
+
+function dropMenu(selector) {
+    //console.log(selector);
+    selector.addEventListener("click", () => {
+        let dropdownMenu = selector.querySelector(".dropdown-menu");
+        dropdownMenu.classList.contains("active") ? dropdownMenu.classList.remove("active") : dropdownMenu.classList.add("active");
+    });
+}
+
+// Sidebar Toggle / bars
+let sidebar = document.querySelector(".sidebar");
+let bars = document.querySelector(".bars");
+
+bars.addEventListener("click", () => {
+    sidebar.classList.contains("active") ? sidebar.classList.remove("active") : sidebar.classList.add("active");
+});
+
+window.matchMedia("(max-width: 768px)").matches ? sidebar.classList.remove("active") : sidebar.classList.add("active");
