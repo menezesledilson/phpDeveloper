6045d2f4c030c92981118b20c1b4efded352260d
menezesledilson <ledilsonmenezes@gmail.com>
2024-05-20 08:47:58 -0300

    layouts

---
+++ TFD5/adm/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Celke - Administrativo</title>
+    </head>
+    <body>
+        <?php echo" Bem vindo ao ADM!";?>
+    </body>
+</html>
