258a9763f0576571736d133ecaa1b5a5f34dc670
menezesledilson <ledilsonmenezes@gmail.com>
2024-05-20 19:06:22 -0300

    formulario

--- layout_adm/js/custom_adm.js
+++ layout_adm/js/custom_adm.js
@@ -1,4 +1,4 @@
-// Dropdown Navbar
+/* Inicio Dropdown Navbar */
 let notification = document.querySelector(".notification");
 let avatar = document.querySelector(".avatar");
 
@@ -12,8 +12,9 @@ function dropMenu(selector) {
         dropdownMenu.classList.contains("active") ? dropdownMenu.classList.remove("active") : dropdownMenu.classList.add("active");
     });
 }
+/* Fim Dropdown Navbar */
 
-// Sidebar Toggle / bars
+/* Inicio Sidebar Toggle / bars */
 let sidebar = document.querySelector(".sidebar");
 let bars = document.querySelector(".bars");
 
@@ -22,3 +23,32 @@ bars.addEventListener("click", () => {
 });
 
 window.matchMedia("(max-width: 768px)").matches ? sidebar.classList.remove("active") : sidebar.classList.add("active");
+/* Fim Sidebar Toggle / bars */
+
+function actionDropdown(id) {
+    closeDropdownAction();
+    document.getElementById("actionDropdown" + id).classList.toggle("show-dropdown-action");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches(".dropdown-btn-action")) {
+        /*document.getElementById("actionDropdown").classList.remove("show-dropdown-action");*/
+        closeDropdownAction();
+    }
+}
+
+function closeDropdownAction() {
+    var dropdowns = document.getElementsByClassName("dropdown-action-item");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i]
+        if (openDropdown.classList.contains("show-dropdown-action")) {
+            openDropdown.classList.remove("show-dropdown-action");
+        }
+    }
+}
+
+
+/* Inicio botao dropdown do listar */
+
+/* Fim botao dropdown do listar */
